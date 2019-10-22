ba2f83757ab35666b7a0550c18330d55f84d7621
cqlboy <cqlboy@hotmail.com>
2019-10-22 11:20:07 -0700

    commit latest production release

--- uspEmployee_upd.StoredProcedure.sql
+++ uspEmployee_upd.StoredProcedure.sql
@@ -1,29 +1,25 @@
 
-
-/****** Object:  StoredProcedure [dbo].[uspEmployee_ins]    Script Date: 10/18/2019 4:38:48 PM ******/
-SET ANSI_NULLS ON
+if object_id('uspEmployee_upd') is not null
+	drop procedure uspEmployee_upd
 GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-
 
 create procedure [dbo].[uspEmployee_upd]
 
 	@id int
 /*
 ********************* History **************************
-Date  	 Change By	Desc
+Date  	 Change By		Desc
 -------- -------------	--------------------------------
-20191021 BColson	Created
+20191021 BColson		Created.
+20191022 BColson		Add comments + WHERE.
 
 
 *********************************************************/
 as
-
+-- simple update statement
 update Employee
 set fname = 'test'
+WHERE id = 1
 GO
 
 
